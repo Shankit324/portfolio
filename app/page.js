07d58860b6c04a93324547bf4dba6ff1fe5c4cd3
Shankit324 <165839266+Shankit324@users.noreply.github.com>
2024-09-07 14:17:39 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -108,12 +108,12 @@ export default function Home() {
       </h2></div><br />
       <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <a class="y" style={{ backgroundColor: "rgba(250, 246, 250, 0.2)", textDecoration: "none", color: "black",  display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", width: "100%", justifyItems: "center", width: "80%", borderRadius: "10px"  }} href="https://github.com/Shankit324/chatroom.git">
-        <div class="y1" style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", margin: "5px 5px 5px 5px" }}>
-        <img class="img1" src="/chat.png" style={{ width: "30%", height: "250px", borderRadius: "5px", margin: "15px 15px 15px 15px" }}></img>
-          <div class="div" style={{ width: "40%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <div class="y1" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", margin: "5px 5px 5px 5px" }}>
+          <div class="div" style={{ width: "80%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
             <h2 class="underline" style={{ color: "#c18f32", fontSize: "25px", fontWeight: "500" }}>
               CHATROOM
             </h2>
+            <img class="img1" src="/chat.png" style={{ width: "50%", height: "50%", borderRadius: "5px", margin: "15px 15px 15px 15px" }}></img>
             <p style={{ color: "white", textAlign: "left" }}>
               A chat room is an online platform that enables users to communicate with each other in real time. Chat rooms are typically hosted on a server with an internet connection, enabling members from around the world to hold conversations about various topics.
             </p>
