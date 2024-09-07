884f87abe83bb352886a316354ad08b11f2e53cf
Shankit324 <165839266+Shankit324@users.noreply.github.com>
2024-09-07 14:19:54 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -109,7 +109,7 @@ export default function Home() {
       <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <a class="y" style={{ backgroundColor: "rgba(250, 246, 250, 0.2)", textDecoration: "none", color: "black",  display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", width: "100%", justifyItems: "center", width: "80%", borderRadius: "10px"  }} href="https://github.com/Shankit324/chatroom.git">
         <div class="y1" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", margin: "5px 5px 5px 5px" }}>
-          <div class="div" style={{ width: "80%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+          <div class="div" style={{ width: "80%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
             <h2 class="underline" style={{ color: "#c18f32", fontSize: "25px", fontWeight: "500" }}>
               CHATROOM
             </h2>
